911fae00f17ad4bdc19c06b3f8a01ca3c51988b0
Esteban SEMELLIER <esteban.semellier@mobiapps.fr>
2023-11-20 14:25:49 +0100

    first commit

---
+++ FaceApp/LockView.swift
@@ -0,0 +1,8 @@
+//
+//  LockView.swift
+//  FaceApp
+//
+//  Created by Esteban SEMELLIER on 20/11/2023.
+//
+
+import Foundation
---
+++ FaceApp/ViewModels/LockViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LockViewModel.swift
+//  FaceApp
+//
+//  Created by Esteban SEMELLIER on 20/11/2023.
+//
+
+import Foundation
