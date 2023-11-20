db2c7111851bee955f14b76519679e4bbdae0728
Esteban SEMELLIER <esteban.semellier@mobiapps.fr>
2023-11-20 13:43:00 +0100

    Initial Commit

---
+++ FaceApp/FaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAppApp.swift
+//  FaceApp
+//
+//  Created by Esteban SEMELLIER on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
